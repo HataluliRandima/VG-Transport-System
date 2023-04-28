7cf0ad7197fed80dbb7251b001a7a5da8db4faa3
Hataluli Randima <100038171+HataluliRandima@users.noreply.github.com>
2023-04-28 16:17:38 +0200

    added the feedback controller

--- VG TransportAPI/VG TransportAPI/Controllers/FeedBacksController.cs
+++ VG TransportAPI/VG TransportAPI/Controllers/FeedBacksController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Win32;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using VG_TransportAPI.DTO.FeedBack;
 using VG_TransportAPI.Interfaces;
 using VG_TransportAPI.Models;
 
@@ -27,6 +30,46 @@ namespace VG_TransportAPI.Controllers
             this._context = _context;
             this._configuration = _configuration;
             _emailServices = emailServices;
+        }
+
+        //add feed back by the customer 
+        [HttpPost]
+        [Route("addfeedC")]
+        public async Task<IActionResult> feedbackcust([FromBody]CustomerFeed fs )
+        {
+
+            try
+            {
+                int userID = Convert.ToInt32(HttpContext.User.FindFirstValue("CustomerID"));
+
+                if (fs == null)
+                {
+                    return BadRequest("You cant do feedbacl empty");
+                }
+
+                if (userID == null && userID <= 0)
+                {
+                    return BadRequest("You are not log in , please log in");
+                }
+
+                var newfeed = new Feedback();
+
+                newfeed.CId = userID;
+                newfeed.FMessageC = fs.FMessageC;
+                newfeed.FRateC = fs.FRateC;
+                newfeed.OId = fs.OId;
+
+                _context.Feedbacks.Add(newfeed);
+                await _context.SaveChangesAsync();
+
+                return Ok(new { message = "your feedback has been submitted" });
+
+
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
     }
